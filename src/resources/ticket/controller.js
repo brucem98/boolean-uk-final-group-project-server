75553d47cbb57c03777dcc8578008017604439d7
bruce_milliken <brucemilliken98@gmail.com>
2021-11-12 11:19:05 +0000

    sorting out nested competition object in ticket array

--- src/resources/ticket/controller.js
+++ src/resources/ticket/controller.js
@@ -2,13 +2,38 @@ const prisma = require("../../utils/database");
 
 const getAllTickets = async (req, res) => {
   try {
-    const data = await prisma.ticket.findMany();
+    const data = await prisma.ticket.findMany({
+      include: {
+        competition: true
+      }
+    });
     res.json({ data });
   } catch (error) {
     console.error("[ERROR] getAll: ", { error });
     res.json({ error });
   }
-};
+}
 
 
-module.exports = { getAllTickets };
+const getCompetitionFromOneTicket = async (req, res) => {
+ 
+  try {
+  const data = await prisma.ticket.findUnique({   //You can use .findFirst and .findUnique
+  where: {
+    id: parseInt(req.params.id)
+  },  
+     
+            include: {
+              competition: true
+            }
+     
+  })
+  res.json(data)
+} catch (error) {
+  console.error("[ERROR] getAll: ", { error });
+  res.json({ error });
+}
+}
+
+
+module.exports = { getAllTickets, getCompetitionFromOneTicket };
