aa6071560e1f61d12c30bd82f37e0ba12e0031a1
bruce_milliken <brucemilliken98@gmail.com>
2021-11-15 12:01:10 +0000

    remove ticketNumber from Ticket prisma model

---
+++ prisma/migrations/20211115120032_remove_ticket_number_from_ticket/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ticketNumber` on the `Ticket` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Ticket" DROP COLUMN "ticketNumber";
